f349d7fae1d9bf438cfae20e0dfe3ec3c239de8f
dsolisav <dsolis@unal.edu.co>
2024-07-28 00:48:29 -0500

    Removed unused imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import CustomNavbar from "@/components/CustomNavbar";
 import { NextUIProvider } from "@nextui-org/react";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
